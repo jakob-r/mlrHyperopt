c67d5c67aa8b4ab9452b0a1031c2d73ad4e23ad3
Bernd Bischl <bernd_bischl@gmx.net>
2014-05-09 21:27:12 +0200

    fix parsing of PCS file

--- R/parseParamFile.R
+++ R/parseParamFile.R
@@ -10,38 +10,38 @@ parseParamFile = function(scen) {
 
   index.cond = which(str_detect(lines, "Conditionals:"))
   # do we have conditionals? then restrict to 1st part of file
-  if (length(index.cond) > 1L)
+  if (length(index.cond) > 0L)
     lines1 = lines[1:(index.cond-1)]
   else
     lines1 = lines
 
   for (i in seq_along(lines1)) {
     line = lines[i]
-    print(line)
-    #FIXME: this is wrong, we need to consume the name with regexp
-    line = splitAndTrim(line, " ", n = 2L)
-    id = line[1L]
-    rest = line[-1L]
-    if (str_detect(rest, "^\\[.*\\]")) {
+    # print(line)
+    z = consume(line, "[a-zA-Z0-9]+\\s*")
+    id = str_trim(z$match)
+    if (str_detect(z$rest, "^\\[.*\\]")) {
       # num or int param
-      z = consume(rest, "^\\[.*?\\]")
+      z = consume(z$rest, "^\\[.*?\\]")
       bounds = removeChars(z$match, c("\\[", "\\]"))
       bounds = splitAndTrim(bounds, ",", convert = as.numeric)
-      if (str_detect(rest, "i"))
+      if (str_detect(z$rest, "i"))
         par = makeIntegerParam(id = id, lower = bounds[1L], upper = bounds[2L])
       else
         par = makeNumericParam(id = id, lower = bounds[1L], upper = bounds[2L])
-    } else if (str_detect(rest, "^\\{.*\\}")) {
+    } else if (str_detect(z$rest, "^\\{.*\\}")) {
       # discrete
-      constraints = str_extract(rest, "^\\{.*\\}")
+      constraints = str_extract(z$rest, "^\\{.*\\}")
       values = removeChars(constraints, c("\\{", "\\}"))
       values = splitAndTrim(values, ",")
       par = makeDiscreteParam(id = id, values = values)
+    } else {
+      stop("Should not happen!")
     }
     pars[[id]] = par
   }
-  pars = makeParamSet(params = pars)
-  return(pars)
+
+  makeParamSet(params = pars)
 }
 
 
