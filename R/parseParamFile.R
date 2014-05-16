2d8eac4396fc9ed894320f6d40fbcdb371ddd149
Bernd Bischl <you@example.com>
2014-05-16 22:38:19 +0200

    improve parsing of PCS

--- R/parseParamFile.R
+++ R/parseParamFile.R
@@ -6,6 +6,7 @@ parseParamFile = function(scen) {
   lines = readTxtTrimAndRemove(path)
   lines = removeComments(lines)
   result = list()
+  param.requires = list()
 
   # lines with | are conditions on params
   j = str_detect(lines, "\\|")
@@ -21,14 +22,40 @@ parseParamFile = function(scen) {
   lines = lines[!str_detect(lines, "Conditionals:")]
 
   lines.params = lines
-  
-  #FIXME: remove debug prints 
 
+  #FIXME: remove debug prints
+
+  # parse conditionals first
+  for (i in seq_along(lines.cond)) {
+    line = lines.cond[i]
+    # print(line)
+    s = str_split(line, "\\|")[[1L]]
+    # print(s)
+    id = str_trim(s[1L])
+    # print(id)
+    req = str_trim(s[2L])
+    # transform to R expression and parse
+    req = str_replace(req, "in", "%in%")
+    req = str_replace(req, "\\{", "c(")
+    req = str_replace(req, "\\}", ")")
+    req = parse(text = req)
+    print(req)
+    param.requires[[id]] = req
+  }
+ 
+  #FIXME: can it happen that we have more than 1 condition for a param? probably yes! we need to add them up!
+   
+  # parse param lines
   for (i in seq_along(lines.params)) {
     line = lines.params[i]
     # print(line)
     z = consume(line, "[a-zA-Z0-9_\\-]+\\s*")
     id = str_trim(z$match)
+
+    # get conditional
+    req = param.requires[[id]]
+    param.requires[[id]] = NULL
+
     if (str_detect(z$rest, "^\\[.*\\]")) {
       # num or int param
       z = consume(z$rest, "^\\[.*?\\]")
@@ -43,7 +70,7 @@ parseParamFile = function(scen) {
       }
       def = parseDefault(z$rest, convert = defConv)
       # print(str(def))
-      par = makeP(id = id, lower = bounds[1L], upper = bounds[2L], default = def)
+      par = makeP(id = id, lower = bounds[1L], upper = bounds[2L], default = def, requires = req)
     } else if (str_detect(z$rest, "^\\{.*\\}")) {
       # discrete
       z = consume(z$rest, "^\\{.*\\}")
@@ -52,11 +79,17 @@ parseParamFile = function(scen) {
       # print(z$rest)
       def = parseDefault(z$rest)
       # print(def)
-      par = makeDiscreteParam(id = id, values = values, default = def)
+      par = makeDiscreteParam(id = id, values = values, default = def, requires = req)
     } else {
       stop("Should not happen!")
     }
     result[[id]] = par
+  }
+
+  # there should no conditionals be left
+  if (length(param.requires) > 0L) {
+    print(param.requires)
+    stop("Should not happen!")
   }
 
   makeParamSet(params = result)
