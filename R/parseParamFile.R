73718e15aa15519df320942eae0e148f17045bee
Bernd Bischl <you@example.com>
2014-05-16 22:24:01 +0200

    improve parsing of PCS

--- R/parseParamFile.R
+++ R/parseParamFile.R
@@ -17,7 +17,12 @@ parseParamFile = function(scen) {
   lines.forbidden = lines[j]
   lines = lines[!j]
 
+  # remove Conditionals start line
+  lines = lines[!str_detect(lines, "Conditionals:")]
+
   lines.params = lines
+  
+  #FIXME: remove debug prints 
 
   for (i in seq_along(lines.params)) {
     line = lines.params[i]
@@ -29,18 +34,24 @@ parseParamFile = function(scen) {
       z = consume(z$rest, "^\\[.*?\\]")
       bounds = removeChars(z$match, c("\\[", "\\]"))
       bounds = splitAndTrim(bounds, ",", convert = as.numeric)
-      makeP = if (str_detect(z$rest, "i"))
-        makeIntegerParam
-      else
-        makeNumericParam
-      def = parseDefault(z$rest)
+      if (str_detect(z$rest, "i")) {
+        makeP = makeIntegerParam
+        defConv = as.integer
+      } else {
+        makeP = makeNumericParam
+        defConv = as.numeric
+      }
+      def = parseDefault(z$rest, convert = defConv)
+      # print(str(def))
       par = makeP(id = id, lower = bounds[1L], upper = bounds[2L], default = def)
     } else if (str_detect(z$rest, "^\\{.*\\}")) {
       # discrete
       z = consume(z$rest, "^\\{.*\\}")
       values = removeChars(z$match, c("\\{", "\\}"))
       values = splitAndTrim(values, ",")
+      # print(z$rest)
       def = parseDefault(z$rest)
+      # print(def)
       par = makeDiscreteParam(id = id, values = values, default = def)
     } else {
       stop("Should not happen!")
@@ -52,7 +63,8 @@ parseParamFile = function(scen) {
 }
 
 parseDefault = function(s, convert = as.character) {
-  s = removeChars(s, c("\\[", "\\]"))
+  s = consume(s, "\\[.*\\]")
+  s = str_trim(removeChars(s$match, c("\\[", "\\]")))
   convert(s)
 }
 
